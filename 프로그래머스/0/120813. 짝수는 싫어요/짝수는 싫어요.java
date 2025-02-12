7e9f06025bfea948a3d5b4e528112164f8e6a4cb
Audrey <100057254+Audrey-1120@users.noreply.github.com>
2025-02-12 14:14:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,26 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int n) {
-        
-        int[] answer = {};
-        
-        // 홀수의 개수는 n이 짝수일때 n/2(개) / n이 홀수일때 (n+1)/2(개)
-        if(n % 2 == 0) {
-            answer = new int[n/2];
-        } else {
-            answer = new int[(n+1)/2];
+        int[] answer = new int[(n + 1) / 2];
+
+        for (int i = 0, num = 1; num <= n; i++, num += 2) {
+            answer[i] = num; 
         }
-        
-        // n 이하의 홀수 구하기
-        int index = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 != 0) {
-                answer[index] = i;
-                index++;
-            }
-        }
-        
+
         return answer;
     }
 }
