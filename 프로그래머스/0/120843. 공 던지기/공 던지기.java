a6114dee9a5a6f19865d8600f571ad014ca31067
Audrey <100057254+Audrey-1120@users.noreply.github.com>
2025-02-26 11:56:03 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int count = 0; // 횟수
+        int index = 0; // 현재 위치
+        
+        while(count < k) {
+            if(index >= numbers.length) {
+                index %= numbers.length;
+            }
+            answer = numbers[index];
+            index += 2;
+            count++;
+        }
+        return answer;
+    }
+}
